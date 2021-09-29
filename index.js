1fa886ce9e2f2bd0c27489e48e1493d2262aee53
Tomexar <tommyexar@gmail.com>
2021-09-29 17:38:33 -0600

    for heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('./api/server')
 
 
-server.listen(4000, () =>{
-    console.log('server running on 4k')
+PORT = '7777'
+
+server.listen(PORT, () =>{
+    console.log(`server running on ${PORT}`)
 })
